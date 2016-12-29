6846e49f47cf81ce287c4833a1552317fb99971e
Josh Bryans <joshhsoj1902@gmail.com>
2016-12-28 22:26:42 -0500

    Update config.inc.php

--- ogp_web/ogp/config/config.inc.php
+++ ogp_web/ogp/config/config.inc.php
@@ -1,11 +1,1 @@
-<?php
-###############################################
-# Site configuration
-###############################################
-$db_host="my_db";
-$db_user="ogpuser";
-$db_pass="KwC6sXqAlTV";
-$db_name="opengamepanel";
-$table_prefix="ogp_";
-$db_type="mysql";
-?>
+
