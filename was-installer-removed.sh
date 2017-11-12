3341b49049594456b3ffafa8b8361d2e7c39ce51
josh <joshhsoj1902@gmail.com>
2017-11-11 22:08:45 -0500

    fix test

--- was-installer-removed.sh
+++ was-installer-removed.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ ! -f /var/www/html/install.php ]; then
-    echo "File not found!"
-    exit 1
-else
-    echo "File Found"
+    echo "File not found, PASS!"
     exit 0
+else
+    echo "File Found, FAIL"
+    exit 1
 fi
