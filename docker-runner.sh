b7acdce99b82fc6583b53cede655ec8d3ba96a52
Josh Bryans <joshhsoj1902@gmail.com>
2017-11-11 20:38:29 -0500

    Auto configure web (#7)
    * Give this container to ability to configure itself!
    
    * Keep install.php if the autoinstall fails
    
    * cleanup
    
    * Test that file exists
    
    * Force test to fail
    
    * FORCE TEST FAILURE
    
    * USe Curl to test
    
    * automated tests
    
    * test again
    
    * Banana
    
    * banana 2
    
    * fix tests
    
    * add debug logs
    
    * add echos
    
    * add fail to curl
    
    * orange
    
    * peach
    
    * FORCE FAIL
    
    * try again
    
    * debug
    
    * :(
    
    * comeback to the testing...

--- docker-runner.sh
+++ docker-runner.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
+if [ -z "$MYSQL_IP" ];        then  echo "Need to set MYSQL_IP environment";        exit 1; fi
+if [ -z "$MYSQL_USER" ];      then  echo "Need to set MYSQL_USER environment";      exit 1; fi
+if [ -z "$MYSQL_PASS" ];      then  echo "Need to set MYSQL_PASS environment";      exit 1; fi
+if [ -z "$ADMIN_USERNAME" ];  then  echo "Need to set ADMIN_USERNAME environment";  exit 1; fi
+if [ -z "$ADMIN_PASSWORD" ];  then  echo "Need to set ADMIN_PASSWORD environment";  exit 1; fi
+if [ -z "$ADMIN_EMAIL" ];     then echo "Need to set ADMIN_EMAIL environment";      exit 1; fi
+
 if [ -z "$OGP_INSTALL" ] || [ $OGP_INSTALL = 'false' ]; then
   echo "Starting OGP web"
 
-  rm /var/www/html/install.php
+  mv /var/www/html/install.php /var/www/html/install.php.bac
   /usr/local/bin/gomplate -f /var/templates/config.inc.php.tmpl -o /var/www/html/includes/config.inc.php
 else
   echo "Running in install mode"
 
+  /usr/local/bin/gomplate -f /var/templates/config.inc.php.tmpl -o /var/www/html/includes/config.inc.php
+
+
   touch /var/www/html/includes/config.inc.php
   chmod 777 /var/www/html/includes/config.inc.php
   cp /var/www/html/install.php.bac /var/www/html/install.php
+
+  #run installer in background so that it happens after the webserver is up
+  sh -c "./installer.sh" &
+
 fi
 
 
 ./my_init
+
+echo "HERE"
---
+++ installer.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+database_host=${MYSQL_IP:-my_db}
+database_user=${MYSQL_USER:-ogpuser}
+database_pass=${MYSQL_PASS}
+database_name=${MYSQL_DBNAME:-opengamepanel}
+database_prefix=${MYSQL_PREFIX:-_ogb}
+
+admin_username=${ADMIN_USERNAME}
+admin_password=${ADMIN_PASSWORD}
+admin_email=${ADMIN_EMAIL}
+
+
+if [ -z "$OGP_INSTALL" ] || [ $OGP_INSTALL = 'false' ]; then
+  echo "Not in INSTALL mode"
+else
+  echo "Starting auto install"
+
+  sleep 15 #Give the db container time to start up
+  timeout 60s sh -c 'until [ -n "$(curl -ks --head http://localhost)" ]; do echo "waiting for service to start"; sleep 1; done'
+
+  #Setup tables
+  curl --data "db_host=$database_host&db_user=$database_user&db_pass=$database_pass&db_name=$database_name&table_prefix=$database_prefix&next=next" http://localhost/install.php?step=2
+  sleep 2
+
+  #Setup Admin user
+  #TODO: we could also do some DB checks BEFORE we do this.
+  curl --data "step=3&username=$admin_username&password1=$admin_password&password2=$admin_password&email=$ADMIN_EMAIL&next=next" http://localhost/install.php?step=2
+
+  sleep 2
+
+  if curl -v --silent --data "lang=-&ulogin=$admin_username&upassword=$admin_password&login=Login" http://localhost/index.php 2>&1 | grep -q 'Invalid username or password'; then
+    echo "Login failed. Auto Install likely failed"
+    exit 1
+    echo "Done Grepping 1" 
+  fi
+
+  mv /var/www/html/install.php /var/www/html/install.php.bac
+
+  #TODO: Shuld we also insert the agent?
+
+  echo "Done installing"
+fi
